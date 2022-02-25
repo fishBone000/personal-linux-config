9a20e8e611ddb2a2e9fd0650009d23299273d6f8
fishBone000 <FishBoneEK@outlook.com>
2022-02-25 10:36:10 +0800

    Added mlocate

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 USR='fishbone'
 cp -t /home/$USR/ .vimrc .bash_aliases
-apt install manpages-posix manpages-posix-dev gdb vim git aptitude bsdgames vsftpd net-tools
+apt install manpages-posix manpages-posix-dev gdb vim git aptitude bsdgames vsftpd net-tools mlocate
